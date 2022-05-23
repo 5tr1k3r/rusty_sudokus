a98ec2e7fa93c2ef6ffacaacfa8482321c5604de
Jabarton <noblemonitor@gmail.com>
2022-05-23 17:40:59 +0500

    add batch solving

--- src/sudoku_solver.rs
+++ src/sudoku_solver.rs
@@ -3,6 +3,8 @@ use crate::puzzle::Puzzle;
 use crate::tech::base_tech::Technique;
 use crate::tech::hidden_single::HiddenSingle;
 use crate::tech::single_candidate::SingleCandidate;
+use std::fs;
+use std::time::Instant;
 
 fn solve(puzzle: &mut Puzzle) -> bool {
     let techs: Vec<Box<dyn Technique>> = vec![
@@ -34,6 +36,60 @@ fn solve(puzzle: &mut Puzzle) -> bool {
     is_validated
 }
 
+fn batch_solve(filename: String) {
+    let all_puzzles_string = fs::read_to_string(&filename).expect("File reading error");
+    let all_puzzles = all_puzzles_string.lines();
+
+    let mut total_count = 0;
+    let mut unsolved_count = 0;
+    let time_start = Instant::now();
+
+    for puzzle_string in all_puzzles {
+        let mut puzzle = Puzzle::from_string(puzzle_string.to_string());
+
+        total_count += 1;
+        if !solve(&mut puzzle) {
+            unsolved_count += 1;
+        }
+    }
+
+    let time_taken = time_start.elapsed().as_secs_f64();
+
+    let result_string = construct_result_string(&filename, total_count, unsolved_count, time_taken);
+    println!("{}", result_string);
+}
+
+fn batch_solve_everything() {
+    let files = [
+        "batches/0.txt",
+        "batches/1.txt",
+        "batches/2.txt",
+        "batches/3.txt",
+        "batches/5.txt",
+    ];
+    for file in files {
+        batch_solve(file.to_string());
+    }
+}
+
+fn construct_result_string(
+    filename: &String,
+    total_count: u32,
+    unsolved_count: u32,
+    time_taken: f64,
+) -> String {
+    let mut output: Vec<String> = vec![format!("{}", filename)];
+
+    let unsolved_rate: f32 = unsolved_count as f32 / total_count as f32;
+
+    output.push(format!(
+        "Total: {}, unsolved: {} ({}), took {}s",
+        total_count, unsolved_count, unsolved_rate, time_taken
+    ));
+
+    output.join("\n")
+}
+
 fn notify_applying_technique(tech: &Box<dyn Technique>) {
     if SOLVE_OUTPUT_ENABLED {
         println!("Applying {} technique", tech.get_name());
@@ -51,12 +107,15 @@ fn notify_solution_invalid() {
 }
 
 pub fn run() {
-    let pstring: String =
-        "030072001000030090518000003050203100000705306000640205200060014007000630000008900"
-            .to_string();
-    let mut my_puzzle = Puzzle::from_string(pstring);
+    // let pstring: String =
+    //     "030072001000030090518000003050203100000705306000640205200060014007000630000008900"
+    //         .to_string();
+    // let mut my_puzzle = Puzzle::from_string(pstring);
 
-    solve(&mut my_puzzle);
+    // solve(&mut my_puzzle);
 
-    dbg!(my_puzzle);
+    // dbg!(my_puzzle);
+
+    // batch_solve("batches/5.txt".to_string());
+    batch_solve_everything();
 }
